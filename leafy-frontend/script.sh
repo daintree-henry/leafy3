860a1907c9e7485d746476aed55ef2f9ff27d476
devwiki-henry <devwiki.henry@gmail.com>
2025-01-07 22:34:14 +0900

    leafy-front -> leafy-frontend

--- leafy-frontend/script.sh
+++ leafy-frontend/script.sh
@@ -3,5 +3,5 @@ docker build -t (개인레지스트리명)/leafy-frontend:1.0.0 . --no-cache
 docker push (개인레지스트리명)/leafy-frontend:1.0.0 
 
 # 2. 컨테이너 실행 및 로그 확인, localhost 접속 테스트
-docker run -d -p 80:80 --name leafy-front --network leafy-network (개인레지스트리명)/leafy-frontend:1.0.0 
-docker logs -f leafy-front
+docker run -d -p 80:80 --name leafy-frontend --network leafy-network (개인레지스트리명)/leafy-frontend:1.0.0 
+docker logs -f leafy-frontend
